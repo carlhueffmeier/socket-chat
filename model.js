f89896235b2917c93c39946839451c3b48dd67fa
Carl Hüffmeier <carlhueffmeier@gmail.com>
2018-08-14 22:31:17 +0200

    fix: Encoding

--- model.js
+++ model.js
@@ -2,9 +2,10 @@ const mysql = require('mysql');
 const SQL = {
   CREATE_MESSAGE_TABLE: `CREATE TABLE if not exists messages(
     message_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    author VARCHAR(30) NOT NULL,
-    message VARCHAR(500) NOT NULL
+    author VARCHAR(30) CHARACTER SET utf8mb4 NOT NULL,
+    message VARCHAR(500) CHARACTER SET utf8mb4 NOT NULL
   );`,
+  SET_ENCODING: `SET NAMES 'utf8mb4';`,
   INSERT_MESSAGE: `INSERT INTO 
   messages(author, message) VALUE(?, ?);`,
   ALL_MESSAGES: `SELECT author, message FROM messages;`
@@ -12,12 +13,14 @@ const SQL = {
 
 const pool = mysql.createPool({
   connectionLimit: 10,
+  charset: 'utf8mb4',
   ...require('./config.js').mySqlConnectionSettings
 });
 
 pool.query(SQL.CREATE_MESSAGE_TABLE, err => {
   if (err) throw err;
 });
+pool.query(SQL.SET_ENCODING);
 
 exports.getAllMessages = () => {
   return new Promise((resolve, reject) => {
